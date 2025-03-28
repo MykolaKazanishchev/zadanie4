5e0a5be37d29b89378c886f3177c2f415dc3f620
s33582 <s33582@pjwstk.edu.pl>
2025-03-28 16:57:49 +0100

    Zrobione

--- Klient.java
+++ Klient.java
@@ -6,15 +6,22 @@ public class Klient {
     private String email;
     private ArrayList<Wydarzenie> listaRezerwacji;
 
+
     public Klient(String imie, String nazwisko, String email) {
         this.imie = imie;
         this.nazwisko = nazwisko;
         this.email = email;
+        this.listaRezerwacji = new ArrayList<>();
     }
 
-    public Klient(String imie, String nazwisko) {
+
+    public Klient(String imie, String nazwisko, ArrayList<Wydarzenie> listaRezerwacji) {
         this.imie = imie;
         this.nazwisko = nazwisko;
+        this.listaRezerwacji = new ArrayList<>();
+        if (listaRezerwacji != null) {
+            this.listaRezerwacji.addAll(listaRezerwacji);
+        }
     }
 
     public String getImie() {
@@ -46,32 +53,35 @@ public class Klient {
     }
 
     public void setListaRezerwacji(ArrayList<Wydarzenie> listaRezerwacji) {
-        this.listaRezerwacji = listaRezerwacji;
+        if (listaRezerwacji == null) {
+            this.listaRezerwacji = new ArrayList<>();
+        } else {
+            this.listaRezerwacji = listaRezerwacji;
+        }
     }
 
-    public void dodajRezerwacji(Wydarzenie wydarzenie) {
+    public void dodajRezerwacje(Wydarzenie wydarzenie) {
         listaRezerwacji.add(wydarzenie);
-        System.out.println("Rezerwacja na wydarzenie: " + wydarzenie.getNazwa() + "została dodana.");
+        System.out.println("Rezerwacja na wydarzenie: " + wydarzenie.getNazwa() + " została dodana.");
     }
 
     public void wyswietlRezerwacje() {
-        if(listaRezerwacji.isEmpty()) {
+        if (listaRezerwacji.isEmpty()) {
             System.out.println("Brak zarezerwowanych wydarzeń");
-        }
-        else {
+        } else {
             System.out.println("Zarezerwowane wydarzenia:");
-            for(Wydarzenie w : listaRezerwacji) {
+            for (Wydarzenie w : listaRezerwacji) {
                 System.out.println(w);
             }
         }
     }
+
     public void anulujRezerwacje(Wydarzenie wydarzenie) {
-        if(listaRezerwacji.contains(wydarzenie)) {
+        if (listaRezerwacji.contains(wydarzenie)) {
             listaRezerwacji.remove(wydarzenie);
-            System.out.println("Rezerwacja na wydarzenie: " + wydarzenie.getNazwa() + "została anulowana.");
-        }
-        else {
-            System.out.println("Wydarzenie: " + wydarzenie.getNazwa() + "nie zostało zarezerwowane.");
+            System.out.println("Rezerwacja na wydarzenie: " + wydarzenie.getNazwa() + " została anulowana.");
+        } else {
+            System.out.println("Wydarzenie: " + wydarzenie.getNazwa() + " nie zostało zarezerwowane.");
         }
     }
 }
--- SystemRezerwacji.java
+++ SystemRezerwacji.java
@@ -5,71 +5,59 @@ public class SystemRezerwacji {
     private ArrayList<Klient> listaKlientow;
 
     public SystemRezerwacji() {
-        listaWydarzen = new ArrayList<>();
-        listaKlientow = new ArrayList<>();
+        this.listaWydarzen = new ArrayList<>();
+        this.listaKlientow = new ArrayList<>();
     }
+
     public void dodajWydarzenie(Wydarzenie wydarzenie) {
         listaWydarzen.add(wydarzenie);
-        System.out.println("Wydarzenie: " + wydarzenie.getNazwa() + " zostalo dodane.");
+    }
 
+    public void dodajWydarzenie(String nazwa, double cena) {
+        listaWydarzen.add(new Wydarzenie(nazwa, cena));
     }
-    public void dodajWydarzenie(String nazwa, double cena){
-        Wydarzenie wydarzenie = new Wydarzenie(nazwa, cena);
-        listaWydarzen.add(wydarzenie);
-        System.out.println("Wydarzenie: " + nazwa + " zostalo dodane.");
-    }
+
     public void dodajKlienta(Klient klient) {
         listaKlientow.add(klient);
-        System.out.println("Klient: " + klient.getImie() + " " + klient.getNazwisko() + " został dodany.");
     }
 
-
     public void dodajKlienta(String imie, String nazwisko, String email) {
-        Klient klient = new Klient(imie, nazwisko, email);
-        listaKlientow.add(klient);
-        System.out.println("Klient: " + imie + " " + nazwisko + " został dodany.");
-    }}
+        listaKlientow.add(new Klient(imie, nazwisko, email));
+    }
 
+    public boolean dokonajRezerwacji(Klient klient, Wydarzenie wydarzenie) {
+        if (wydarzenie.getDostepneMiejsca() > 0) {
+            wydarzenie.zarezerwujMiejsce();
+            klient.dodajRezerwacje(wydarzenie);
+            return true;
+        }
+        return false;
+    }
 
-//    public void dokonajRezerwacji(Klient klient, Wydarzenie wydarzenie) {
-//        if (wydarzenie.getDostepneMiejsca() > 0) {
-//            klient.dokonajRezerwacje(wydarzenie);
-//            wydarzenie.zarezerwujMiejsce();
-//            System.out.println("Rezerwacja na wydarzenie: " + wydarzenie.getNazwa() + " dla klienta " + klient.getImie() + " " + klient.getNazwisko() + " została dokonana.");
-//        } else {
-//            System.out.println("Brak dostępnych miejsc na wydarzenie: " + wydarzenie.getNazwa());
-//        }
-//    }
-//
-//
-//    public Wydarzenie znajdzWydarzenie(String nazwa) {
-//        for (Wydarzenie w : listaWydarzen) {
-//            if (w.getNazwa().equals(nazwa)) {
-//                return w;
-//            }
-//        }
-//        System.out.println("Nie znaleziono wydarzenia o nazwie: " + nazwa);
-//        return null;
-//    }
-//
-//
-//    public Klient znajdzKlienta(String nazwisko) {
-//        for (Klient k : listaKlientow) {
-//            if (k.getNazwisko().equals(nazwisko)) {
-//                return k;
-//            }
-//        }
-//        System.out.println("Nie znaleziono klienta o nazwisku: " + nazwisko);
-//        return null;
-//    }
-//
-//
-//    public void zmienCeneWydarzenia(String nazwa, double nowaCena) {
-//        Wydarzenie wydarzenie = znajdzWydarzenie(nazwa);
-//        if (wydarzenie != null) {
-//            wydarzenie.setCena(nowaCena);
-//            System.out.println("Cena wydarzenia: " + nazwa + " została zmieniona na: " + nowaCena + " PLN.");
-//        }
-//    }
-//}
-//
+    public Wydarzenie znajdzWydarzenie(String nazwa) {
+        for (Wydarzenie wydarzenie : listaWydarzen) {
+            if (wydarzenie.getNazwa().equalsIgnoreCase(nazwa)) {
+                return wydarzenie;
+            }
+        }
+        return null;
+    }
+
+    public Klient znajdzKlienta(String nazwisko) {
+        for (Klient klient : listaKlientow) {
+            if (klient.getNazwisko().equalsIgnoreCase(nazwisko)) {
+                return klient;
+            }
+        }
+        return null;
+    }
+
+    public boolean zmienCeneWydarzenia(String nazwa, double nowaCena) {
+        Wydarzenie wydarzenie = znajdzWydarzenie(nazwa);
+        if (wydarzenie != null) {
+            wydarzenie.setCena(nowaCena);
+            return true;
+        }
+        return false;
+    }
+}
